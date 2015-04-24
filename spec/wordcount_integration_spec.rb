c28e675be5ba463b5d4072894bdd4533da98aaeb
leroi <leroidejesa@the-cave.local>
2015-04-24 14:42:27 -0700

    fix small bug in integration_spec

--- spec/wordcount_integration_spec.rb
+++ spec/wordcount_integration_spec.rb
@@ -4,10 +4,10 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the wordcount path', {:type => :feature}) do
-  it('processes the user entry of a word as well as the user entry of a sentence, and returns the number of occurrances said word makes in said sentence')
+  it('processes the user entry of a word as well as the user entry of a sentence, and returns the number of occurrances said word makes in said sentence') do
     visit('/')
     fill_in('word', :with =>'happy')
-    fill_in('string_sentence', :with => 'I am so very happy, happy, happy to be getting my code reviewed today.')
+    fill_in('string_sentence', :with =>'I am so very happy, happy, happy to be getting my code reviewed today.')
     click_button('Find out!')
     expect(page).to have_content('3')
   end
