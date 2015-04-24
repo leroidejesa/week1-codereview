b627645ad1ba2c55f970545bf8f7bd916218cd03
leroi <leroidejesa@the-cave.local>
2015-04-24 10:48:42 -0700

    first set of logic ideas. not passing

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,5 +1,27 @@
 class String
-  define_method(:wordcount) do
-    #workin on it!
+  define_method(:wordcount) do |givenstring|
+
+    array_sentence = |givenstring|.split()
+    numberofwords = 0
+    array_sentence.each() do #|wordtocount?|
+      if array_sentence.include?(self)
+        numberofwords += 1
+      end
+    end
+    #other ideas
+    # sentence_hash = Hash.new()
+    # array_sentence.each() do |wordtopush|
+    #   sentence_hash.store(wordtopush, )
+    # end
+    #hash idea. scratched
+
+    # numberofwords = 0
+    # until numberofwords.==(givenstring) do
+    #   numberofwords += 1
+    # end
+    #until loop idea. scratched
+
+
+    numberofwords
   end
 end
--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -2,5 +2,8 @@ require('rspec')
 require('wordcount')
 
 describe('String#wordcount') do
-  #workin on it!
+  it("returns how frequently it appears in the argument") do
+    expect(("happy").wordcount()).to(eq("3"))
+  end
+#if argument String reads "I am so very happy, happy, happy to be getting my code reviewed today."
 end
